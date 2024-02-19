406f05fd08f3b684d80dba4e643ddd6fb001e187
BrittanyBrown14 <121870967+BrittanyBrown14@users.noreply.github.com>
2024-02-19 13:37:29 +0000

    changed config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
-// export const API_URL = window.location.hostname === "localhost" ? "http://127.0.0.1:8181" : "http://127.0.0.1:8181";
-export const API_URL = window.location.hostname;
+ export const API_URL = window.location.hostname === "localhost" ? "http://127.0.0.1:8181" : "http://127.0.0.1:8181";
 console.log(
     "API_URL :",
     API_URL

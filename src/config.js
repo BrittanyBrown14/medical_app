22ec806ba9700a4820e0c32d624464a37dfabde3
BrittanyBrown14 <121870967+BrittanyBrown14@users.noreply.github.com>
2024-02-19 13:13:19 +0000

    website now runs on server that is connected to mongodb

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
-export const API_URL = window.location.hostname === "localhost" ? "http://127.0.0.1:8181" : "http://127.0.0.1:8181";
+// export const API_URL = window.location.hostname === "localhost" ? "http://127.0.0.1:8181" : "http://127.0.0.1:8181";
+export const API_URL = window.location.hostname;
 console.log(
     "API_URL :",
     API_URL
